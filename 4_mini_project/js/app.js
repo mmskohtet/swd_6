b41375a8b308dd6583546a5882921ae4f4720b62
mmskohtet <heinhtetzan9514@gmail.com>
2020-03-12 14:12:33 +0630

    jquery

--- 4_mini_project/js/app.js
+++ 4_mini_project/js/app.js
@@ -1,4 +1,5 @@
 let rate = exchange.rates;
+let calcHistory = [];
 
 function showCurrencyList(selector) {
     for(let x in rate){
@@ -9,7 +10,7 @@ function showCurrencyList(selector) {
 function checkInput(x){
     let input = Number(x);
     if(!input){
-        alert("Please Input Something")
+        alert("Please Input Something");
     }else{
         return input;
     }
@@ -26,8 +27,44 @@ function toMmk(x,y){
     return calculated;
 }
 
+function del(x){
+    if(confirm("Are Your Sure to Delete Row"+x+" ?")){
+        calcHistory.splice(x,1);
+        showHistory();
+    }
+}
+
 showCurrencyList("from");
 showCurrencyList("to");
+
+function showHistory(){
+    let output = document.getElementById("output");
+    if(calcHistory.length >= 1){
+        output.innerHTML = "";
+        for(let x in calcHistory){
+            output.innerHTML += `<tr>
+                    <td class="text-center">${Number(x)+1}</td>
+                    <td class="text-right">${calcHistory[x].input}</td>
+                    <td class="text-right">${calcHistory[x].from}</td>
+                    <td class="text-right">${calcHistory[x].to}</td>
+                    <td class="text-right">${calcHistory[x].result}</td>
+                    <td class="text-center"><i class="fa fa-trash fa-2x" onclick="del(${x})"></i></td>
+                    <td class="text-right">${calcHistory[x].time}</td>                    
+                </tr>`;
+        }
+        // let start = 1;
+        // for(let x of calcHistory){
+        //     output.innerHTML += `<tr>
+        //             <td class="text-center">${start++}</td>
+        //             <td class="text-center">${x.input}</td>
+        //
+        //
+        //         </tr>`;
+        // }
+    }else{
+        output.innerHTML = `<tr><td colspan="7">There is no History</td></tr>`;
+    }
+}
 
 
 function calc(){
@@ -37,13 +74,36 @@ function calc(){
     let from = document.getElementById("from").value;
     let to = document.getElementById("to").value;
 
-    //process
-    console.log(`${input} ${from} ${to}`);
-    // toMmk(input,from);from
-    let mmk = toMmk(input,from);
-    let calculated = mmk / trueValue(to);
+    if(input && from && to){
 
-    //set
-    document.querySelector(".result").innerHTML = `Your Result : ${calculated.toFixed(2)} ${to}`;
+        //process
+        console.log(`${input} ${from} ${to}`);
+        // toMmk(input,from);from
+        let mmk = toMmk(input,from);
+        let calculated = mmk / trueValue(to);
+        let result = `${calculated.toFixed(2)} ${to}`;
+
+        //set
+        document.querySelector(".result").innerHTML = `Your Result : ${result}`;
+
+        let date = new Date();
+        let showTime = date.getHours()+" : "+date.getMinutes();
+        console.log(showTime);
+
+        let currentHistory = {
+            input: input,
+            from:from,
+            to:to,
+            result:result,
+            time: showTime
+        };
+
+        calcHistory.push(currentHistory);
+
+        console.log(calcHistory);
+        showHistory();
+
+    }
+
 
 }
